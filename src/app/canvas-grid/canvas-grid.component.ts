31c17d0f091f86e8343f9a6ba526a925e8120933
romain <yysh@hotmail.fr>
2018-10-20 18:51:31 +0200

    "rationalisation d ela construction de la grille en cours"

--- src/app/canvas-grid/canvas-grid.component.ts
+++ src/app/canvas-grid/canvas-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -10,33 +11,31 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 export class CanvasGridComponent implements OnInit {
 
   @ViewChild('gameOfLife') canvasRef: ElementRef;
-  @ViewChild('canvasContainer') containRef: ElementRef
-
+  @ViewChild("canvasContainer") canvascontainer;
+  
+  private containRef;
   private ctx;
   private width;
   private height;
   private cellsNumb: number;
 
   private defineDimensions(): void{  // redefinition de la taille du canva pour le responsive, en le liant à la taille d'un conteneur;
-    this.width = this.containRef.nativeElement.offsetHeight ;
-    this.height = this.containRef.nativeElement.offsetWidth;
+    this.width = parseInt(this.canvascontainer.nativeElement.offsetHeight) ;
+    this.height = parseInt(this.canvascontainer.nativeElement.offsetWidth);
     this.canvasRef.nativeElement.width =  this.width;
     this.canvasRef.nativeElement.height = this.height
   }
 
   private drawInit(){ //déssine la grille
     this.ctx = this.canvasRef.nativeElement.getContext("2d");
-    this.cellsNumb = 55; //nombre magique en attendant de décider si oui on non on peut configurer le nombre de cellules.
-    for (var i = this.width/this.cellsNumb; i < this.width-(this.width/this.cellsNumb); i+= this.width/this.cellsNumb) {
-      for (var n = this.height/this.cellsNumb; n < this.height-(this.height/this.cellsNumb); n+= this.width/this.cellsNumb) {
-        this.ctx.strokeRect((n-(this.height/this.cellsNumb)),i,(this.height/this.cellsNumb),(this.height/this.cellsNumb));
-            this.ctx.strokeStyle= "black" //une double boucle for qui déssine une case se décale de la hauteur de la case et ainsi de suite, colonne par colonne(la première boucle for change de colonne)
-            this.ctx.fillStyle= "white"
-            
-            this.ctx.fillRect((n-(this.height/this.cellsNumb)),i,(this.height/this.cellsNumb),(this.height/this.cellsNumb));
+    this.cellsNumb = 52; //nombre magique en attendant de décider si oui on non on peut configurer le nombre de cellules.
+    this.buildGrid("white", false);
+  }
+  private fillInit() {
+    var startingCells = Math.random(); // TODO mettre cette partie dans configuration pour regler le pourcentage de départ
+    if (startingCells <= 0.5 ) {
 
-      }
-    }
+    } 
   
   }
    // va initialiser la grille et le remplir par le biais d'autres fonctions.
@@ -58,6 +57,21 @@ export class CanvasGridComponent implements OnInit {
   getWidth() {
     return this.width;
     }
+  private buildGrid(color: string, state: boolean) {
+      for (var i = this.width/this.cellsNumb; i < this.width-(this.width/this.cellsNumb); i+= this.width/this.cellsNumb) {
+        for (var n = this.height/this.cellsNumb; n < this.height-(this.height/this.cellsNumb); n+= this.width/this.cellsNumb) {
+        this.cell(i,n, color, state);
+        }
+      }
+    }
+  private cell(i,n,color,state) {
+    this.ctx.strokeRect((n-(this.height/this.cellsNumb)),i,(this.height/this.cellsNumb),(this.height/this.cellsNumb));
+    this.ctx.strokeStyle= "black" //une double boucle for qui déssine une case se décale de la hauteur de la case et ainsi de suite, colonne par colonne(la première boucle for change de colonne)
+    this.ctx.fillStyle= color
+    this.ctx.fillRect((n-(this.height/this.cellsNumb)),i,(this.height/this.cellsNumb),(this.height/this.cellsNumb));
+  
   }
+
+  
 
 
