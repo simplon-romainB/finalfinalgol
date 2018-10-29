670e6e6e0555c07359f57ce9a5a1ea48619e414f
romain <yysh@hotmail.fr>
2018-10-29 08:42:14 +0100

    factorisation et rationalisation de la mécanique du calcul des cells

--- src/app/canvas-grid/canvas-grid.component.ts
+++ src/app/canvas-grid/canvas-grid.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { CellsNumbTabModel} from ".././cells-numb-tab-model";
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CellsNumbTabModel } from ".././cells-numb-tab-model";
 
 
 
@@ -19,11 +17,12 @@ export class CanvasGridComponent implements OnInit {
   private ctx;
   private width;
   private height;
+  private finalCells;
   private cellsNumb: number;
   private cellsNumbTabModelsPrevious: CellsNumbTabModel[] = [];  //TOFIX faire un tableau à deux dimensions plutot
   private cellsNumbTabModelsNext: CellsNumbTabModel[] = [];
   private count: any = 0 //TOFIX trouver une autre solution pour numéroter les cases
-  private comptage: any;
+  
   
   private defineDimensions(): void{  // redefinition de la taille du canva pour le responsive, en le liant à la taille d'un conteneur;
 
@@ -56,26 +55,41 @@ export class CanvasGridComponent implements OnInit {
     return this.width;
     }
   private buildGrid(init) {
-       this.comptage = -1; 
-       //TOFIX se debarasser de cette var comptage qui en plus recourt à un nombre magique
+       this.count = -1; 
+       if (init == false) {
+        console.log(this.cellsNumbTabModelsPrevious);
+       }
+       //TOFIX se debarasser de cette var count qui en plus recourt à un nombre magique
        //une double boucle for qui déssine une case se décale de la hauteur de la case et ainsi de suite, colonne par colonne(la première boucle for change de colonne)
       for (var i = this.width/this.cellsNumb; i < this.width-(this.width/this.cellsNumb); i+= this.width/this.cellsNumb) {
         for (var n = this.height/this.cellsNumb; n < this.height-(this.height/this.cellsNumb); n+= this.width/this.cellsNumb) {
           if (init == true) {
           this.setColorsBuild(i,n);
           }
-          else {
-          this.comptage ++;
-          this.cellsNumbTabModelsNext = [];
+        
+          else if (init == false){
+          this.count++;
           this.isAlive(i,n);
-          this.cell(i,n,"yellow");
+          console.log(this.cellsNumbTabModelsNext);
           }
         }
-        
       }
-      init = false;
-      this.comptage = 0;
-    }
+        
+      
+          if (init == false) {
+            for (var i = this.width/this.cellsNumb; i < this.width-(this.width/this.cellsNumb); i+= this.width/this.cellsNumb) {
+              for (var n = this.height/this.cellsNumb; n < this.height-(this.height/this.cellsNumb); n+= this.width/this.cellsNumb) {
+              this.cell(i,n, this.cellsNumbTabModelsNext[this.count].state);
+            }
+              }
+          
+      
+      
+     // this.cellsNumbTabModelsPrevious = this.cellsNumbTabModelsNext;
+      //this.cellsNumbTabModelsNext = [];
+          }
+          init = false;
+  }
   private cell(i,n,color: string) {
     this.ctx.strokeRect((n-(this.height/this.cellsNumb)),i,(this.height/this.cellsNumb),(this.height/this.cellsNumb));
     this.ctx.strokeStyle= "black" 
@@ -92,51 +106,53 @@ export class CanvasGridComponent implements OnInit {
             this.count ++;
       }
       else if (c >= 0.5) {
-            this.cell(i,n, "red")
+            this.cell(i,n, "white")
             this.cellsNumbTabModelsPrevious.push({order: this.count , state: "white"})
             this.count ++
       }
-     console.log(this.cellsNumbTabModelsPrevious) ;
+    console.log(this.cellsNumbTabModelsPrevious);
+     
   }
-  private isAlive(i,n) {
-       if (this.cellsNumbTabModelsPrevious[this.comptage].state == "black") {
-        var alive = 0;
-        if ((this.cellsNumbTabModelsPrevious[this.comptage+1].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage+1].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage-1].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage-1].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage+this.cellsNumb].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage+this.cellsNumb].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage-this.cellsNumb].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage-this.cellsNumb].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage+(this.cellsNumb+1)].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage+(this.cellsNumb+1)].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage-(this.cellsNumb)+1].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage-(this.cellsNumb)+1].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage+(this.cellsNumb)-1].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage+(this.cellsNumb)-1].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage-(this.cellsNumb)-1].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage-(this.cellsNumb)-1].state!= undefined)){alive ++}
-        if ((alive == 2 || alive == 3)) {
-        this.cellsNumbTabModelsNext.push(this.cellsNumbTabModelsPrevious[this.comptage].order)
-        this.cellsNumbTabModelsNext[this.comptage].state == "black";
-      }
-  }
-      if (this.cellsNumbTabModelsPrevious[this.comptage].state == "black") {
-        var alive = 0;
-        if ((this.cellsNumbTabModelsPrevious[this.comptage+1].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage+1].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage-1].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage-1].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage+this.cellsNumb].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage+this.cellsNumb].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage-this.cellsNumb].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage-this.cellsNumb].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage+(this.cellsNumb+1)].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage+(this.cellsNumb+1)].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage-(this.cellsNumb)+1].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage-(this.cellsNumb)+1].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage+(this.cellsNumb)-1].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage+(this.cellsNumb)-1].state!= undefined)){alive ++}
-        if ((this.cellsNumbTabModelsPrevious[this.comptage-(this.cellsNumb)-1].state == "black") && (this.cellsNumbTabModelsPrevious[this.comptage-(this.cellsNumb)-1].state!= undefined)){alive ++}
-        if (alive == 3 ) {
-        this.cellsNumbTabModelsNext.push(this.cellsNumbTabModelsPrevious[this.comptage].state)
-        this.cellsNumbTabModelsNext[this.comptage].state == "black";
+  private isDefined(one, two, three, four, five, six, seven, eight) {
+    this.finalCells = [];
+    for(let i = 0; i < arguments.length; i++) {
+      if (arguments[i] !== undefined) {
+        this.finalCells.push(arguments[i]);
       }
     }
+
+  }
+  private isAlive(i,n) {
+      this.isDefined((this.cellsNumbTabModelsPrevious[this.count-1].state),(this.cellsNumbTabModelsPrevious[this.count+1].state), (this.cellsNumbTabModelsPrevious[this.count+this.cellsNumb].state), (this.cellsNumbTabModelsPrevious[this.count-this.cellsNumb].state), (this.cellsNumbTabModelsPrevious[this.count-(this.cellsNumb)+1].state), (this.cellsNumbTabModelsPrevious[this.count-(this.cellsNumb+1)].state), (this.cellsNumbTabModelsPrevious[(this.count+this.cellsNumb-1)].state), (this.cellsNumbTabModelsPrevious[this.count-(this.cellsNumb)-1].state))
+       if (this.cellsNumbTabModelsPrevious[this.count].state == "black") {
+        var alive = 0;
+      this.finalCells.map(function(x){if (x == "black") {alive++}});
+       
+      }
+    
+        if (alive == 2 || alive == 3) {
+          this.cellsNumbTabModelsNext.push({order: this.count, state: "black"})
+        if ((alive !=2 && alive !=3)) {
+          this.cellsNumbTabModelsNext.push({order: this.count, state: "white"})
+      }
   
-  console.log(this.cellsNumbTabModelsNext);
-  
-  
-  
+      if (this.cellsNumbTabModelsPrevious[this.count].state == "white") {
+        var alive = 0;
+        this.finalCells.map(function(x){if (x == "black") {alive++}});
+      }
+    }
+        if (alive == 3 ) {
+        this.cellsNumbTabModelsNext.push({order: this.count, state: "black"})
+        if (alive !=3) {
+        this.cellsNumbTabModelsNext.push({order: this.count, state: "white"});
+        }
+      }
+      this.finalCells = [];
+      }
   
   }
-}
+  
+
 
   
 
